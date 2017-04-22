c68e66414368fc4f67420b753490205228442a2a
Matthieu <contact@matthieudurand.fr>
2017-04-22 13:15:27 +0200

    json Article

---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,61 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+
+use AppBundle\Entity\Article;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+/**
+ * Description of ArticleController
+ *
+ * @author matthieudurand
+ * @Route("/articles")
+ */
+class ArticleController extends Controller {
+    
+    public function indexAction(){
+        
+    }
+    
+    /**
+     * @Route()
+     * @Method("GET|POST")
+     */
+    public function addAction(){
+        
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $article = new Article();
+        
+        $article->setName("Toto");
+        $article->setPrice("100");
+        $article->setSize("S");
+        $article->setCategory("Pantalon");
+        $article->setMaterials("Chino");
+        $article->setColors("Blue");
+        $article->setBrand("Sandro");
+        $article->setShop("Sandro");
+        $article->setSoldBy("Martin");
+        $article->setSoldAt("2017-04-22T00:00:00");
+        
+        $data = $serializer->serialize($article, 'json');
+        
+        return new Response($data);
+        
+//        return $this->render('article/index.html.twig', [
+//            'article' => $article,
+//            'json' => $data,
+//            ]
+//        );
+        
+    }
+}
