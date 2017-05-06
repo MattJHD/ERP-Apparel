31253011784c6c3c31855de09d740909b151da45
Matthieu <contact@matthieudurand.fr>
2017-05-06 14:29:04 +0200

    api/doc

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        //$root = $this->get('kernel')->getRootDir() . '/web/api/doc';
+        
+        return $this->render('default/index.html.twig',[
+        ]);
     }
     
 }
