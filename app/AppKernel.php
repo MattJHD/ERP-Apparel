6f5600af5053eb8a64f8ad8e61ca85c81b391ad8
Matthieu <contact@matthieudurand.fr>
2017-06-06 23:04:10 +0200

    Lexik JWT Authentication configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
